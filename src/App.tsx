f6134d6eb32070d9e79f00c608a24d06c58e389f
Ignacio Grillo Caimary <igrilloc@pop-os.localdomain>
2023-08-16 01:02:41 -0300

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,12 @@ import { Results } from './Results'
 import { useQuestionsData } from './hooks/useQuestionsData'
 import { useQuestionsStore } from './store/questions'
 
-
-
 function App () {
-  
   const questions = useQuestionsStore(state => state.questions)
   const { unanswered } = useQuestionsData()
   const theme = useTheme()
 
-  const medium = useMediaQuery(theme.breakpoints.up("md"));
+  const medium = useMediaQuery(theme.breakpoints.up("md"))
 
   return (
     <main>
@@ -32,7 +29,7 @@ function App () {
         </Stack>
 
         <strong style={{ display: 'block', fontSize: '14px', marginTop: '5px', marginBottom:'20px' }}>Desarrollado con TypeScript + Zustand - <a style={{ color: 'yellow' }} href='https://github.com/igrilloc/javascript-quiz'>Ir al código</a></strong>
-        
+
         {questions.length === 0 && <Start />}
         {questions.length > 0 && unanswered > 0 && <Game />}
         {questions.length > 0 && unanswered === 0 && <Results />}
